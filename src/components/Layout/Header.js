ecd8832166f28e9bcd297676bc877c3daf7d1c8f
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-05 21:45:16 +0530

    Module4_Task3

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,6 +3,7 @@ import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
+
 const Header = (props) => {
   return (
     <React.Fragment>
