3a341c64f814a960de40fce4bde24dcb5abb983a
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-06 11:23:19 +0530

    Module4_Task5

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Card = (props) => {
+  const cartitems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartitems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Card;
