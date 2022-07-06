76a9cf49b63dd1d513bfeebe285c4081f3c3dd86
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-06 12:45:18 +0530

    Module4_Task7

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Fragment, useState } from "react";
+import React, {useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import Cart from './components/Cart/Cart';
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
-} 
+}
 
 export default App;
