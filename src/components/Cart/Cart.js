7e8f3b22af030e7915aa1ee476d918eeed93d325
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-06 11:46:53 +0530

    Module4_Task6

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
-const Card = (props) => {
+const Cart = (props) => {
   const cartitems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -12,18 +12,18 @@ const Card = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClick={props.onClose}>
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
   );
 };
 
-export default Card;
+export default Cart;
